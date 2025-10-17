e1e752e91516af37a81ee654c1f3f94b373cc9d2
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-17 16:17:16 +0200

    generate random number for points and fouls

--- snack 2/assets/js/main.js
+++ snack 2/assets/js/main.js
@@ -13,11 +13,35 @@ falli subiti e stampiamo tutto in console.
 
 //Create array of objects
 //-every object has name, points and fouls
+const soccerTeams = [
+    {
+        name: 'Juventus',
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: 'Inter',
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: 'Milan',
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: 'Napoli',
+        points: 0,
+        fouls: 0
+    }
+];
 
 
 //Generate random numbers for points and fouls property
-
 //-For in cycle to reassign property values
-
+for (let key in soccerTeams) {
+    soccerTeams[key].points = Math.floor(Math.random() * 100);
+    soccerTeams[key].fouls = Math.floor(Math.random() * 50);
+}
 //Create a new array
 //Push objects with names and generated fouls in the array
