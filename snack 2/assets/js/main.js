ba84b330962a3c6b3d521561176349fd98c22b7e
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-17 16:11:59 +0200

    pseudo code

--- snack 2/assets/js/main.js
+++ snack 2/assets/js/main.js
@@ -1,1 +1,23 @@
 console.log('It works');
+
+/*
+Snack2**
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli
+subiti”.Infine, creiamo un nuovo array i cui elementi contengono solo nomi e 
+falli subiti e stampiamo tutto in console.
+*/
+
+
+//Create array of objects
+//-every object has name, points and fouls
+
+
+//Generate random numbers for points and fouls property
+
+//-For in cycle to reassign property values
+
+//Create a new array
+//Push objects with names and generated fouls in the array
